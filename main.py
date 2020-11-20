e10ed53eef41eb9db216c114f800a3751abe5fdb
Comforter11 <71063972+Comforter11@users.noreply.github.com>
2020-11-20 06:13:32 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# import matplotlib.pyplot as plt
+# temperature = [14.2, 16.5, 11.8, 15.3, 18.8, 22.5, 19.5]
+# soup_sales = [220.00, 330.00, 190.00, 340.00, 410.00, 445.00, 415.00]
+# plt.scatter(temperature, soup_sales)
+# plt.title("Soup sales in relation to temperature.")
+# plt.xlabel("Temperature")
+# plt.ylabel("Price in (R)")
+# plt. plot(temperature + soup_sales)
+# plt.show()
+
+#from playsound import playsound
+#playsound ("Sia - Cheap Thrills (Performance Edit).mp3")
+
+try:
+    x = 1/0
+    print("Dividing 1 by zero.")
+finally:
+    print("I will always run, no matter what!")
+
